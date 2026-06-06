1d52dc69a42e0dbfbb7ccad63616b644e534f33a
JCazores <john.christian.azores2016@gmail.com>
2026-06-07 00:52:16 +0800

    Use env vars for DB connection

--- initialize.php
+++ initialize.php
@@ -3,9 +3,9 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 if(!defined('base_url')) define('base_url','https://smartmed-production-5c92.up.railway.app/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER', 'acela.proxy.rlwy.net');
-if(!defined('DB_PORT')) define('DB_PORT', 58509);
-if(!defined('DB_USERNAME')) define('DB_USERNAME', 'root');
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD', 'oCZnrPaBlUHwYSSosvPTWAFRnKiSwQJI');
-if(!defined('DB_NAME')) define('DB_NAME', 'smart_med');
+if(!defined('DB_SERVER')) define('DB_SERVER', getenv('DB_HOST') ?: 'acela.proxy.rlwy.net');
+if(!defined('DB_PORT')) define('DB_PORT', intval(getenv('DB_PORT') ?: 58509));
+if(!defined('DB_USERNAME')) define('DB_USERNAME', getenv('DB_USER') ?: 'root');
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD', getenv('DB_PASS') ?: 'oCZnrPaBlUHwYSSosvPTWAFRnKiSwQJI');
+if(!defined('DB_NAME')) define('DB_NAME', getenv('DB_NAME') ?: 'smart_med');
 ?>
