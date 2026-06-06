e385633d109fb6b09a60dc7e6e6e7439b583d0c8
JCazores <john.christian.azores2016@gmail.com>
2026-06-07 00:45:17 +0800

    Fix DB connection with public host and port

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -3,19 +3,18 @@ if(!defined('DB_SERVER')){
     require_once("../initialize.php");
 }
 class DBConnection{
-
     private $host = DB_SERVER;
     private $username = DB_USERNAME;
     private $password = DB_PASSWORD;
     private $database = DB_NAME;
+    private $port = DB_PORT;
     
     public $conn;
     
     public function __construct(){
-
         if (!isset($this->conn)) {
             
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
             
             if (!$this->conn) {
                 echo 'Cannot connect to database server';
