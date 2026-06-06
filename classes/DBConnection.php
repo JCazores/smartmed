6988196f5e7b24e609e89fb5a617f8258d0640dc
JCazores <john.christian.azores2016@gmail.com>
2026-06-07 00:59:19 +0800

    Fix DBConnection to use constants directly

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -3,30 +3,21 @@ if(!defined('DB_SERVER')){
     require_once("../initialize.php");
 }
 class DBConnection{
-    private $host = DB_SERVER;
-    private $username = DB_USERNAME;
-    private $password = DB_PASSWORD;
-    private $database = DB_NAME;
-    private $port = DB_PORT;
-    
     public $conn;
     
     public function __construct(){
         if (!isset($this->conn)) {
-            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
             $this->conn = new mysqli();
             $this->conn->real_connect(
-                $this->host,
-                $this->username,
-                $this->password,
-                $this->database,
-                $this->port,
-                null,
-                MYSQLI_CLIENT_SSL
+                DB_SERVER,
+                DB_USERNAME,
+                DB_PASSWORD,
+                DB_NAME,
+                DB_PORT
             );
             
-            if (!$this->conn) {
-                echo 'Cannot connect to database server';
+            if ($this->conn->connect_error) {
+                echo 'Cannot connect to database server: ' . $this->conn->connect_error;
                 exit;
             }            
         }    
